031bfc90b4719e6e82d25bd18a6aca955f680983
glaysoncampos <50781779+glaysoncampos@users.noreply.github.com>
2026-09-17 16:58:39 -0300

    Add configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BASE_URL = "https://coren-go.implanta.net.br/portaltransparencia/"
+
+TARGET_SECTION = "Remuneração de Empregados"
+
+ANO_INICIAL = 2026
+
+OUTPUT_DIR = Path("docs")
+
+TIMEOUT = 60
